b9672011f8b5281dcc73c2367597becbb63f0982
Tomi Alagbe <tomialagbe>
2020-11-16 04:03:03 +0200

    fix pom errors

--- run-consumer.sh
+++ run-consumer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar ../consumer/target/consumer-1.0-SNAPSHOT.jar server ../tokenizer/consumer-config.yml
+java -jar consumer/target/consumer-1.0-SNAPSHOT.jar server consumer/consumer-config.yml
--- run-tokenizer.sh
+++ run-tokenizer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar ../tokenizer/target/tokenizer-1.0-SNAPSHOT.jar server ../tokenizer/tokenizer-config.yml
+java -jar tokenizer/target/tokenizer-1.0-SNAPSHOT.jar server tokenizer/tokenizer-config.yml
