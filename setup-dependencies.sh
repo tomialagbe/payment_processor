312badca7497d518922dfbef8461a09467d81775
Tomi Alagbe <tomialagbe@yahoo.com>
2020-11-17 11:56:50 +0200

    update build

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic data-input
-kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic data-output
+docker exec -it kafka-broker kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic data-input
+docker exec -it kafka-broker kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic data-output
